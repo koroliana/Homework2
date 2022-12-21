45d344db778f916dab348c1fb03386d854501a4e
Elena Koroleva <koroliana@yandex.ru>
2022-12-21 21:30:04 +0700

    Tasks 4-5

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,11 @@ public class Main {
         task1(cat, dog, paper);
         task2(cat, dog, paper);
         task3(cat, dog, paper);
+        task4();
+        task5();
     }
     public static void task1 (double cat, double dog, int paper) {
         System.out.println("Задача 1");
-        Main.cat = cat;
-        Main.dog = dog;
-        Main.paper = paper;
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
@@ -21,9 +20,6 @@ public class Main {
 
     public static void task2 (double cat, double dog, int paper) {
         System.out.println("Задача 2");
-        Main.cat = cat;
-        Main.dog = dog;
-        Main.paper = paper;
         var n = 4;
         dog = dog + n;
         cat = cat + n;
@@ -36,16 +32,34 @@ public class Main {
 
     public static void task3 (double cat, double dog, int paper) {
         System.out.println("Задача 3");
-        Main.cat = cat;
-        Main.dog = dog;
-        Main.paper = paper;
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
         System.out.println();
     }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println("friend = " + friend);
+        friend = friend + 2;
+        System.out.println("friend = " + friend);
+        friend = friend / 7;
+        System.out.println("friend = " + friend);
+        System.out.println();
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println("frog = " + frog);
+        frog = frog*10;
+        System.out.println("frog = " + frog);
+        frog = frog/3.5;
+        System.out.println("frog = " + frog);
+        frog = frog + 4;
+        System.out.println("frog = " + frog);
+        System.out.println();
+    }
+
 
 }
